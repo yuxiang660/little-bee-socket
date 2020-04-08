bf05f93720df7c33b604e8a5119ab9e4e9f58e22
ben <yuxiang660@163.com>
2020-04-08 11:52:16 +0800

    cancel default log file

--- src/internal/lib/easylogging++.h
+++ src/internal/lib/easylogging++.h
@@ -16,6 +16,10 @@
 
 #ifndef EASYLOGGINGPP_H
 #define EASYLOGGINGPP_H
+
+// Custom Macro
+#define ELPP_NO_DEFAULT_LOG_FILE
+
 // Compilers and C++0x/C++11 Evaluation
 #if __cplusplus >= 201103L
 #define ELPP_CXX11 1
